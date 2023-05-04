33a9d33235fc13a897ee5535d4af987556664b84
bhushandulam1984 <bhushandulam@hotmail.com>
2023-05-04 07:31:37 +0530

    added main.tf file

---
+++ Main.tf
@@ -0,0 +1,8 @@
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+Output"password" {
+    value=random_password.password.result
+}
